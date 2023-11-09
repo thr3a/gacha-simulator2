0cd34bf5305f466f1f64377ae93ab025d9f7ffb2
thr3a <thr3a@example.com>
2023-11-10 00:37:56 +0900

    update: セレクトボックスにして操作しやすく

--- src/features/form/AttemptsInput.tsx
+++ src/features/form/AttemptsInput.tsx
@@ -1,40 +1,31 @@
-import { Button, Group, TextInput, Box } from '@mantine/core';
+import { Box, Select } from '@mantine/core';
 import { useGachaFormContext } from '@/features/form/GachaContext';
-import { useEffect } from 'react';
+
+const attemptList = (): string[] => {
+  const list = [];
+  for (let i = 1; i <= 20; i++) {
+    list.push((i * 10).toString());
+  }
+  return list;
+};
 
 export const AttemptsInput = (): JSX.Element => {
   const form = useGachaFormContext();
 
-  const addAttempts = (attempts: number): void => {
-    const prev = form.values.attempts;
-    if (Number.isInteger(prev)) {
-      form.setFieldValue('attempts', prev + attempts);
-    } else {
-      form.setFieldValue('attempts', attempts);
+  const handleChange = (value: any): void => {
+    if (value !== null) {
+      form.setFieldValue('attempts', Number(value));
     }
   };
 
-  useEffect(() => {
-    form.isValid();
-  }, [form.values.attempts]);
-
   return (
     <Box>
-      <TextInput
+      <Select
         label="ガチャを試す回数"
-        {...form.getInputProps('attempts')}
+        data={attemptList()}
+        value={form.values.attempts.toString()}
+        onChange={handleChange}
       />
-      <Group>
-        <Button.Group>
-          <Button variant="outline" onClick={() => { addAttempts(10); }} >+10</Button>
-          <Button variant="outline" color="red" onClick={() => { addAttempts(-10); }}>-10</Button>
-        </Button.Group>
-        <Button.Group>
-          <Button variant="outline" onClick={() => { addAttempts(100); }} >+100</Button>
-          <Button variant="outline" color="red" onClick={() => { addAttempts(-100); }}>-100</Button>
-        </Button.Group>
-
-      </Group>
     </Box>
   );
 };
